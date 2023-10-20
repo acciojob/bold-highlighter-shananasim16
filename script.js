5325d892396da00acded9f29c786d6103dc854e1
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-20 12:32:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
-    //Write your code here
-
-
+    var boldElements = document.querySelectorAll('strong');
+    for (var i = 0; i < boldElements.length; i++) {
+        boldElements[i].classList.add('bold-highlight');
+    }
 }
-
 
 function return_normal() {
-    //Write your code here
-
-    
+    var boldElements = document.querySelectorAll('strong');
+    for (var i = 0; i < boldElements.length; i++) {
+        boldElements[i].classList.remove('bold-highlight');
+    }
 }
+
+var highlightLink = document.getElementById('highlight-link');
+highlightLink.addEventListener('mouseover', highlight);
+highlightLink.addEventListener('mouseout', return_normal);
